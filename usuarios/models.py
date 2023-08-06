6720a36ba7b70c1f83b72db3ec52320c633ff9a1
tatiana-2 <tatianaguzmansaavedra@gmail.com>
2023-08-05 18:27:44 -0600

    Update models.py

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Cliente (class (models.Model): #falta cliente _id auotoinclementable 
+  clie_nombre - models.charfield( max:length-45,   werbose_name- "Nombre Cliente")
+  clie_apellido = models.charfield ( max:lenhth-45, webose:name - "Apellido Cliente")
+  
+
+        
+
+
